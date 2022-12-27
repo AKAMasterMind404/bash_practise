746126a14c962158ecc59fe6fdc0b6bb0f11f3af
Atharv Karbhari <59548859+AKAMasterMind404@users.noreply.github.com>
2022-12-27 10:06:01 +0530

    workable

---
+++ init.sh
@@ -0,0 +1,15 @@
+cd ..
+
+apt update && apt upgrade
+pkg install git
+pkg install tsu
+git clone https://github.com/st42/termux-sudo.git
+cd termux-sudo
+apt install nodejs
+npm install
+npm install express --save
+
+cd termux
+
+mv server.sh ../
+mv delete.sh ../
