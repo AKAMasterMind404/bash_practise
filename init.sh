a083c89780fcc496d42625a00e0ea4e42fbf3d11
Atharv Karbhari <59548859+AKAMasterMind404@users.noreply.github.com>
2022-12-27 10:45:12 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,19 @@ apt update && apt upgrade
 pkg install git
 pkg install tsu
 git clone https://github.com/st42/termux-sudo.git
+
 cd termux-sudo
+
 apt install nodejs
 npm install
 npm install express --save
+
+cd ..
+
+git clone https://github.com/AKAMasterMind404/termux-setup-android.git
+cd termux-android-setup
+
+mv delete.sh ../
+mv server.sh ../
+mv /repos/clone_repo.sh.example ../
+cd ..
